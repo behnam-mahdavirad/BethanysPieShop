8af3f2faddde25af10bf4cc58fad5cb864d4336a
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2020-09-05 23:44:00 +0430

    Added Home View

---
+++ BethanysPieShop/Controllers/Home.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class Home : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public Home(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfWeek = _pieRepository.PiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
